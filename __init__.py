336fd83ff9c2951cf458e9d8c6f767edd7e8180a
booya <github@incognitus.org>
2014-10-19 19:22:03 -0400

    removed commented old code

--- __init__.py
+++ __init__.py
@@ -5,15 +5,3 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 Engine = create_engine('mysql+pymysql://netdot:netdot@192.168.1.242/netdot')
 Session = sessionmaker(bind=Engine)
-
-#class IPBlock(Base):
-#    __tablename__ = 'ipblock'
-#    id = Column(Integer, primary_key=True)
-#    address = Column(Integer)
-#    description = Column(String(128))
-#    prefix = Column(Integer)
-#    version = Column(Integer)
-#
-#    def __repr__(self):
-#        return "IPBlock {}/{}".format(self.address, self.prefix)
-
